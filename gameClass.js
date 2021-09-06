bb66c420172baa38d8afdde84cc6efe3d6f9a2de
HonvedoAdam <honvedoadamkj@gmail.com>
2021-09-06 20:15:56 +0200

    refactor flipCard method

--- gameClass.js
+++ gameClass.js
@@ -8,11 +8,8 @@ let pairsFound = [];
 let cardsArr = Array.from(cardsList);
 
 //declare object for gameFunctions
-
-export const colorsMemoryGame = {
-
-
-
+const colorsMemoryGame = {
+  //Reload page when all the pairs found
   gameOverAction() {
     if (pairsFound.length == 18) {
       setTimeout(() => {
@@ -20,7 +17,7 @@ export const colorsMemoryGame = {
       }, 1000);
     }
   },
-
+ //Randomize order of the cards
   randomizeOrder() {
     cardsArr.map((card) => {
       let randomNumber = Math.floor(Math.random() * 20);
@@ -31,51 +28,6 @@ export const colorsMemoryGame = {
   flipCard(e) {
     e.target.classList.toggle("backSide");
     clickedCards.push(e.target.dataset.name);
-    colorsMemoryGame.actionOnPair();
-    if (pairsFound) {
-      pairsFoundData.innerHTML = `${pairsFound.length / 2} of 9`;
-    }
-  },
-  
-actionIfPair(){
-     //check if pair
-  if (
-    clickedCards[0] === `${clickedCards[1]}2` ||
-    clickedCards[1] === `${clickedCards[0]}2`
-  ) {
-    //handle if pair
-    const card1 = document.querySelector(`[data-name=${clickedCards[0]}]`);
-    const card2 = document.querySelector(`[data-name=${clickedCards[1]}]`);
-    //prevent to click the cards again
-    card1.removeEventListener("click", colorsMemoryGame.flipCard);
-    card2.removeEventListener("click", colorsMemoryGame.flipCard);
-    pairsFound.push(card1);
-    pairsFound.push(card2);
-    clickedCards = [];
-    card1.classList.add("invisible");
-    card2.classList.add("invisible");
-    colorsMemoryGame.gameOverAction();
-  }
-},
-actionIfNotPair(){
-  //check if no pair found
-  if (
-    clickedCards[0] !== `${clickedCards[1]}2` ||
-    clickedCards[1] !== `${clickedCards[0]}2`
-  ) {
-    //handle if not a pair
-    const card1 = document.querySelector(`[data-name=${clickedCards[0]}]`);
-    const card2 = document.querySelector(`[data-name=${clickedCards[1]}]`);
-    setTimeout(() => {
-      if (card1 && card2) {
-        card1.classList.toggle("backSide");
-        card2.classList.toggle("backSide");
-      }
-    }, 1000);
-    clickedCards = [];
-  }
-},
-  actionOnPair() {
     if (clickedCards.length == 2) {
       //prevent user from clicking more cards at the same time
       colorsMemoryGame.removeEventListenerFromCards();
@@ -86,6 +38,48 @@ actionIfNotPair(){
       //check if pair has found
       colorsMemoryGame.actionIfPair();
       colorsMemoryGame.actionIfNotPair();
+    }
+    if (pairsFound) {
+      pairsFoundData.innerHTML = `${pairsFound.length / 2} of 9`;
+    }
+  },
+
+  actionIfPair() {
+    //check if pair
+    if (
+      clickedCards[0] === `${clickedCards[1]}2` ||
+      clickedCards[1] === `${clickedCards[0]}2`
+    ) {
+      //handle if pair
+      const card1 = document.querySelector(`[data-name=${clickedCards[0]}]`);
+      const card2 = document.querySelector(`[data-name=${clickedCards[1]}]`);
+      //prevent to click the cards again
+      card1.removeEventListener("click", colorsMemoryGame.flipCard);
+      card2.removeEventListener("click", colorsMemoryGame.flipCard);
+      pairsFound.push(card1);
+      pairsFound.push(card2);
+      clickedCards = [];
+      card1.classList.add("invisible");
+      card2.classList.add("invisible");
+      colorsMemoryGame.gameOverAction();
+    }
+  },
+  actionIfNotPair() {
+    //check if no pair found
+    if (
+      clickedCards[0] !== `${clickedCards[1]}2` ||
+      clickedCards[1] !== `${clickedCards[0]}2`
+    ) {
+      //handle if not a pair
+      const card1 = document.querySelector(`[data-name=${clickedCards[0]}]`);
+      const card2 = document.querySelector(`[data-name=${clickedCards[1]}]`);
+      setTimeout(() => {
+        if (card1 && card2) {
+          card1.classList.toggle("backSide");
+          card2.classList.toggle("backSide");
+        }
+      }, 1000);
+      clickedCards = [];
     }
   },
   //add event listener to the visible cards (method)
@@ -102,8 +96,11 @@ actionIfNotPair(){
       card.removeEventListener("click", colorsMemoryGame.flipCard);
     });
   },
+  
   prepareGame() {
     colorsMemoryGame.randomizeOrder();
     colorsMemoryGame.addEventListenerToVisibleCards();
   },
 };
+
+colorsMemoryGame.prepareGame();
--- main.js
+++
@@ -1,4 +0,0 @@
-// All functions can be found in ./gameClass.js
-import { colorsMemoryGame } from "./gameClass.js";
-
-colorsMemoryGame.prepareGame();
