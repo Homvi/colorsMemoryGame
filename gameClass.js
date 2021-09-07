680e8896d09180c77d778f1b7d91f360390de31e
HonvedoAdam <honvedoadamkj@gmail.com>
2021-09-07 20:54:13 +0200

    try notification on mobile

--- gameClass.js
+++ gameClass.js
@@ -17,7 +17,17 @@ const colorsMemoryGame = {
   gameOverAction() {
     if (pairsFound.length == 18) {
       setTimeout(() => {
-        alert(`Your score is:${numOfClicks+timeSpentInSeconds}`)
+        navigator.serviceWorker.register("sw.js");
+        Notification.requestPermission(function (result) {
+          if (result === "granted") {
+            navigator.serviceWorker.ready.then(function (registration) {
+              registration.showNotification(
+                `Your score is:-${numOfClicks + timeSpentInSeconds}`
+              );
+            });
+          }
+        });
+        //alert(`Your score is:-${numOfClicks + timeSpentInSeconds}`);
         location.reload();
       }, 1000);
     }
